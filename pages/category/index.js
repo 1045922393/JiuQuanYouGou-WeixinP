6f223838bce143a761f2342a966386350371b8b1
hWY <1045922393@qq.com>
2019-09-25 16:15:40 +0800

    九泉优购v1.02---分类第一页数据获取完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,30 +1,52 @@
 // pages/category/index.js
-import {request} from "../../request/request"
+import { request } from "../../request/request"
 Page({
-  data:{
-    leftNav:[],
-    rightChildren:[],
-    currentIndex:0,
-    scrollPosition:0
+  data: {
+    leftNav: [],
+    rightChildren: [],
+    currentIndex: 0,
+    scrollPosition: 0
   },
-  Catch:[],//直接在在page下设置属性
-  onLoad(){
-    request({
-      url:'/categories'
-    }).then(res=>{
-      this.Catch=res.data.message;
-      this.setData({
-        leftNav:this.Catch.map(item=>{return {cat_name:item.cat_name}}),
-        rightChildren:this.Catch.map(item=>{return item.children})[0]
-      })
-    })
+  Catch: [],//直接在在page下设置属性
+  onLoad() {
+    // let data={value:111}
+    // wx.setStorageSync("YouGou",data)
+    // console.log(wx.getStorageSync("YouGou"))
+    let storage = wx.getStorageSync("YouGou")
+    if (!storage) {
+      this.getCatchFromRemote()
+    } else {
+      let time = Date.now() - storage.time
+      if (time > 60000) {
+        this.getCatchFromRemote()
+      } else {
+        this.Catch = storage.data
+        this.setData({
+          leftNav: this.Catch.map(item => { return { cat_name: item.cat_name } }),
+          rightChildren: this.Catch.map(item => { return item.children })[0]
+        })
+      }
+    }
   },
-  handleTap(e){
+  handleTap(e) {
     let index = e.currentTarget.dataset.index
     this.setData({
-      currentIndex:index,
-      rightChildren:this.Catch.map(item=>{return item.children})[index],
-      scrollPosition:0
+      currentIndex: index,
+      rightChildren: this.Catch.map(item => { return item.children })[index],
+      scrollPosition: 0
+    })
+  },
+  //封装远程拉取数据
+  getCatchFromRemote() {
+    request({
+      url: '/categories'
+    }).then(res => {
+      wx.setStorageSync("YouGou", { time: Date.now(), data: res.data.message })
+      this.Catch = res.data.message;
+      this.setData({
+        leftNav: this.Catch.map(item => { return { cat_name: item.cat_name } }),
+        rightChildren: this.Catch.map(item => { return item.children })[0]
+      })
     })
   }
 })
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,66 +1,50 @@
 // pages/goods_list/index.js
+import {request} from "../../request/request"
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    topTab: [
+      {
+        id: 0,
+        name: '综合'
+      }, {
+        id: 1,
+        name: '销量'
+      }, {
+        id: 2,
+        name: '价格'
+      },
+    ],
+    tabIndex: 0,
+    goodsList:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //  搜索的提交参数
+  searchParams: {
+    query: "",
+    cid: "",
+    pagenum: 1,
+    pagesize: 10
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  goodsTotal:0,
+  toggleTab(e) {
+    this.setData({
+      tabIndex: e.currentTarget.dataset.index
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+// 组件加载完成
+  onLoad(option){
+    this.searchParams.cid=option.cid
+    this.getList()
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getList(){
+    request({
+      url:"/goods/search",
+      data:this.searchParams
+    }).then(res=>{
+      this.setData({
+        goodsList:res.data.message.goods
+      })
+      this.goodsTotal=res.data.message.total
+    })
   }
 })
